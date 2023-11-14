1881713c086b33ede551712d32472c020a1c8904
Aluno_AEDS <Aluno_AEDS@DESKTOP-K339EAF>
2023-11-14 15:55:34 +0000

    Add project files.

---
+++ exercicio 10/Form1.cs
@@ -0,0 +1,32 @@
+namespace exercicio_10
+{
+    using Microsoft.VisualBasic;
+    public partial class Form1 : Form
+    {
+        public string passwordcerta;
+        public string password;
+        public Form1()
+        {
+            InitializeComponent();
+            passwordcerta = "password";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            password = Interaction.InputBox("Por favor, insira a palavra passe:", "Input Box");
+            if (password == passwordcerta)
+            {
+                MessageBox.Show("Password válida");
+            }
+            else
+                MessageBox.Show("Password inválida");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            passwordcerta = Interaction.InputBox("Por favor, insira a nova palavra passe:", "Input Box");
+            MessageBox.Show("Password trocada com sucesso");
+        }
+    }
+}
